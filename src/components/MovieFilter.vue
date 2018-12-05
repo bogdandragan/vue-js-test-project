118ab2450062194c2572709ff02280066d018be2
bogdan <bogdan@dragan.com.ua>
2018-12-05 16:19:15 +0200

    finish project

--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -3,11 +3,11 @@
         <h2>Filter results</h2>
         <h3>By time of day</h3>
         <div class="filter-group">
-            <check-filter v-for="time in times" v-bind:title="time" v-on:check-filter="checkFilter" category="time"></check-filter>
+            <check-filter v-for="time in times" v-bind:title="time" category="time"></check-filter>
         </div>
         <h3>By genre</h3>
         <div class="filter-group">
-            <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter" category="genre"></check-filter>
+            <check-filter v-for="genre in genres" v-bind:title="genre"  category="genre"></check-filter>
         </div>
     </div>
 </template>
@@ -23,9 +23,7 @@
             }
         },
         methods: {
-            checkFilter(category, title, checked) {
-                this.$emit('check-filter', category, title, checked);
-            }
+
         },
         components: {
             CheckFilter
--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import Vue from 'vue';
 import './style.scss';
 
 import genres from './util/genres';
-import MovieList from './components/MovieList.vue';
-import MovieFilter from './components/MovieFilter.vue';
 
 import VueResource from 'vue-resource';
 Vue.use(VueResource);
@@ -12,6 +10,19 @@ import moment from 'moment-timezone'
 moment.tz.setDefault("UTC");
 Object.defineProperty(Vue.prototype, '$moment', {get() {return this.$root.moment} });
 
+import {checkFilter, setDay} from './util/bus'
+const bus = new Vue();
+Object.defineProperty(Vue.prototype, '$bus', { get() {return this.$root.bus} });
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import routes from './util/routes';
+const router = new VueRouter({routes});
+
+import Tooltip from './util/tooltip';
+Vue.use(Tooltip);
+
 new Vue({
     el: "#app",
     data: {
@@ -19,27 +30,20 @@ new Vue({
       time: [],
       movies: [],
       moment,
-      day: moment()  
+      day: moment(),
+      bus
     },
-    methods:{
-      checkFilter(category, title, checked) {
-          if(checked) {
-              this[category].push(title);
-          }else{
-              let index = this[category].indexOf(title);
-              if(index > -1){
-                  this[category].splice(index,1);
-              }
-          }
-      }
-    },
-    components: {
-        MovieList,
-        MovieFilter
-    },
+    methods:{},
     created() {
        this.$http.get("/api").then(response => {
           this.movies = response.data;
        });
-    }
+       this.$bus.$on('check-filter', checkFilter.bind(this));
+       this.$bus.$on('set-day', setDay.bind(this));
+
+    },
+    router
 });
+
+
+
